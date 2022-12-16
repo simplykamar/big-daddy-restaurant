7f6fccd8936a33b7196f4a2ebcbfd3b228999107
simplykamar <kamaralam366@gmail.com>
2022-12-17 00:34:29 +0530

    designed ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,6 @@ import logo2 from '../img/restro/logo2.png'
 const Navbar = () =>{
   return(
     <div>
-    {/*<div className="container-fluid">
-        <div className="row bg-dark text-light pt-2">
-          <div className="col-lg-4 col-md-4 col-sm-4 col-4">
-            <p><i class="fa-solid fa-phone-volume"></i> +91 123456789</p>
-          </div>
-          
-          <div className="col-lg-4 col-md-4 col-sm-4 col-4">
-            <p><i class="fa-solid fa-paper-plane"></i> test@gmail.com</p>
-                
-          </div>
-
-          <div className="col-lg-4 col-md-4 col-sm-4 col-4">
-            <p><i class="fa-solid fa-clock"></i> 10AM to 9PM</p>
-                
-          </div>
-        </div>
-        </div>*/}
-
         <nav className="navbar text-center p-0 navbar-expand-lg fixed-top navbar-light bg-light custom-navbar">
       <div className="container-fluid">
         <a className="navbar-brand font-weight-bolder" href="#">Big Daddy<img src={logo2} className="img-fluid logo"/></a>
@@ -33,7 +15,7 @@ const Navbar = () =>{
           </ul>*/}
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a href="tel:9634142017" className="nav-link"><i class="fa-solid fa-phone-volume"></i> +91 123456789</a>
+            <a href="tel:9634142017" className="nav-link"><i class="fa-solid fa-phone-volume"></i> +91 9634142017</a>
             </li>
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
