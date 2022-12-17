02a6fa5470265a01a3619b9e18e723b3248efc06
simplykamar <kamaralam366@gmail.com>
2022-12-17 15:36:08 +0530

    offer modal added

--- src/App.js
+++ src/App.js
@@ -10,20 +10,22 @@ import Section1 from './components/pages/Section1'
 import Section2 from './components/pages/Section2'
 import Menu from './components/pages/Menu'
 import Footer from './components/pages/Footer'
+import Modal from './components/Modal'
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Modal/>
       <About/>
+
       <SpecialDish/>
       <ExpertChef/>
       <Testimony/>
       <Booking/>
       <Section2/>
       <Menu/>
-
       <Section1/>
       <Footer/>
 
